70dbb4cf150d6d5f3097a3014bf7ad3416e61296
wjhey <102706333+wjhey@users.noreply.github.com>
2022-10-03 14:24:32 +0800

    Update UI.js

--- UI.js
+++ UI.js
@@ -14,7 +14,7 @@ ui.layout(
     <drawer id="drawer">
         <vertical>
             <appbar>
-                <toolbar id="toolbar" bg="#ff4fb3ff" title="强国助手"/>
+                <toolbar id="toolbar" bg="#ff4fb3ff" title="强国助手-"/>
                 <tabs id="tabs" bg="#ff4fb3ff"/>
             </appbar>
             <viewpager id="viewpager">
